3c44d19a534cacb8d143c7abf26eed1e79fbde5a
Oliver <oliver@MSI>
2022-12-27 09:29:45 -0800

    finished jasmine tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,47 @@
-
+describe('monthly-rate tests',function(){
+  it('should calculate the monthly rate correctly', function () {
+  const values = {
+    rate: 6,
+    years: 5,
+    amount: 20000
+  }
+  expect(calculateMonthlyPayment(values)).toBe('386.66');
+});
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    rate: 10,
+    years: 1,
+    amount: 100000
+  }
+  expect(calculateMonthlyPayment(values)).toBe('8791.59');
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it('should calculate the monthly rate correctly', function () {
+  const values = {
+    rate: 2,
+    years: 30,
+    amount: 700000
+  }
+  expect(calculateMonthlyPayment(values)).toBe('2587.34');
 });
+})
 
-/// etc
+
+
+describe('round to 2 decimal places',function(){
+  it('should return result w/ 2 decimal places', function () {
+    const values = {
+      rate: 50,
+      years: 100,
+      amount: 2000
+    }
+    expect(calculateMonthlyPayment(values)).toBe('83.33');
+  });
+  it('should return result w/ 2 decimal places', function () {
+    const values = {
+      rate: 0,
+      years: 0,
+      amount: 0
+    }
+    expect(calculateMonthlyPayment(values)).toBe('NaN');
+  });
+});
